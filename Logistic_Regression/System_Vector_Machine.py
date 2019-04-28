7a11f34b3c38f18e8e4eac6d948430986edc8baa
Dhanasekar <sekard929@gmail.com>
2019-04-28 23:11:20 +0530

    Changes Updated

---
+++ Logistic_Regression/System_Vector_Machine.py
@@ -0,0 +1,105 @@
+import numpy
+import re
+import os
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+
+nltk.download("stopwords")
+
+nltk.download("wordnet")
+
+reviews_train = []
+for line in open('C:\\Users\\DELL5547\\Desktop\\ds\\movie_data\\full_train.txt', 'r', encoding="utf8"):
+    
+    reviews_train.append(line.strip())
+    
+reviews_test = []
+for line in open('C:\\Users\\DELL5547\\Desktop\\ds\\movie_data\\full_test.txt', 'r', encoding="utf8"):
+    
+    reviews_test.append(line.strip())
+
+print (reviews_train[1])
+
+REPLACE_NO_SPACE = re.compile("(\.)|(\;)|(\:)|(\!)|(\')|(\?)|(\,)|(\")|(\()|(\))|(\[)|(\])|(\d+)")
+REPLACE_WITH_SPACE = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
+NO_SPACE = ""
+SPACE = " "
+
+def preprocess_reviews(reviews):
+    
+    reviews = [REPLACE_NO_SPACE.sub(NO_SPACE, line.lower()) for line in reviews]
+    reviews = [REPLACE_WITH_SPACE.sub(SPACE, line) for line in reviews]
+    
+    return reviews
+
+reviews_train_clean = preprocess_reviews(reviews_train)
+reviews_test_clean = preprocess_reviews(reviews_test)
+
+print (reviews_train_clean[1])
+
+english_stop_words = stopwords.words('english')
+def remove_stop_words(corpus):
+    removed_stop_words = []
+    for review in corpus:
+        removed_stop_words.append(
+            ' '.join([word for word in review.split() 
+                      if word not in english_stop_words])
+        )
+    return removed_stop_words
+
+no_stop_words_train = remove_stop_words(reviews_train_clean)
+no_stop_words_test = remove_stop_words(reviews_test_clean)
+
+def get_stemmed_text(corpus):
+    from nltk.stem.porter import PorterStemmer
+    stemmer = PorterStemmer()
+
+    return [' '.join([stemmer.stem(word) for word in review.split()]) for review in corpus]
+
+stemmed_reviews_train = get_stemmed_text(no_stop_words_train)
+stemmed_reviews_test = get_stemmed_text(no_stop_words_test)
+
+def get_lemmatized_text(corpus):
+    
+    from nltk.stem import WordNetLemmatizer
+    lemmatizer = WordNetLemmatizer()
+    return [' '.join([lemmatizer.lemmatize(word) for word in review.split()]) for review in corpus]
+
+lemmatized_reviews_train = get_lemmatized_text(stemmed_reviews_train)
+lemmatized_reviews_test = get_lemmatized_text(stemmed_reviews_test)
+
+ngram_vectorizer = CountVectorizer(binary=True, ngram_range=(1, 2))
+ngram_vectorizer.fit(lemmatized_reviews_train)
+X = ngram_vectorizer.transform(lemmatized_reviews_train)
+X_test = ngram_vectorizer.transform(lemmatized_reviews_test)
+
+#tfidf_vectorizer = TfidfVectorizer()
+#tfidf_vectorizer.fit(reviews_train_clean)
+#X = tfidf_vectorizer.transform(reviews_train_clean)
+#X_test = tfidf_vectorizer.transform(reviews_test_clean)
+
+
+print (X.shape)
+
+target = [1 if i < 12500 else 0 for i in range(25000)]
+
+X_train, X_val, y_train, y_val = train_test_split(
+    X, target, train_size = 0.75
+)
+
+for c in [0.01, 0.05, 0.25, 0.5, 1]:
+    
+    svm = LinearSVC(C=c)
+    svm.fit(X_train, y_train)
+    print ("Accuracy for C=%s: %s" 
+           % (c, accuracy_score(y_val, svm.predict(X_val))))
+
+final_model = LogisticRegression(C=0.05)
+final_model.fit(X, target)
+print ("Final Accuracy: %s" 
+       % accuracy_score(target, final_model.predict(X_test)))
